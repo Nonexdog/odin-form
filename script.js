57261f8bb6455050e40535ff7c029a38f08dcd0e
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2024-01-09 18:53:18 -0600

    Add invalid message to primary password

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const password = document.querySelector('#user_password');
 const passwordConfirm = document.querySelector('#user_password_confirm');
+const regexPwd = /(?=.*(?:=>|<=))(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}/g;
 
 password.addEventListener('input', () => {
   if (password.value.length > 0) {
     passwordConfirm.removeAttribute('disabled', '');
   } else {
     passwordConfirm.setAttribute('disabled', '');
+  }
+  if (!regexPwd.test(password.value)) {
+    password.setCustomValidity("Seal must have a minimum of 7 characters and contain at least: one lowercase and one uppercase letter, one number and one synergy arrow ( <= or => )");
+  } else {
+    password.setCustomValidity('');
   }
 });
 
